9218d87570b19935b277a553376879b648303cb3
Peter Leyva <peter.leyva.bazan@gmail.com>
2018-11-16 01:16:49 -0800

    change method

--- simpson1_3_compuesta.m
+++ simpson1_3_compuesta.m
@@ -6,12 +6,26 @@ funcion = input('Ingrese la funcion:','s'); %el parametro 's' es para que no eva
 f = inline(funcion);%convierte la entrada en una funcion para poderla evaluar
 a = input('Ingrese el numero inferior de la integral:');
 b = input('Ingrese el numero superior de la integral:');
-cantidad = input('Ingrese la cantidad de veces que de sea aplicar el metodo Numero Par:');
-n = 2*cantidad;
+n = input('Ingrese la cantidad de veces que de sea aplicar el metodo Numero Par:');
+%n = 2*cantidad;
 aprox = 0;
 h = (b-a)/n;
-
-for x=1:cantidad
-    aprox = aprox +(h/3) *f(f(a)+4*f(a+h)+f(a+2*h));
+s = zeros(n);
+fprintf('valor de h:%.2f \n',h);
+%fprintf('|\t%d\t|\t%.2f\t|\t%.2f\t|\n',x,a,f(a));
+for x=1:n
+    if(x~=1)
+        s(x) = f(a+h);     
+    else
+        s(x) = f(a);     
+    end
+   a = a+h;
+    %aprox = aprox +(h/3) *f(f(a)+4*f(a+h)+f(a+2*h));
+    %fprintf('|\t%d\t|\t%.2f\t|\t%.2f\t|\n',x,a+h,f(a+h));
+    %a=a+2*h;    
 end
-fprintf('El valor aproximado de la integral es: %f',aprox);
+
+for x=1:n
+    
+end
+%fprintf('El valor aproximado de la integral es: %f',aprox);
